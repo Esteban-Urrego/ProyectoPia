bb8098bf0f517ac828953b521089dd53075d16f4
Esteban-Urrego <jurregoro02@uninpahu.edu.co>
2024-05-05 19:58:17 -0500

    login terminado

--- JS/index.js
+++ JS/index.js
@@ -8,21 +8,22 @@ document.addEventListener('DOMContentLoaded', function() {
 
         imagenes[indiceAnterior].style.opacity = '0'; // Desvanecer la imagen actual
         imagenes[indiceActual].style.opacity = '1'; // Mostrar la siguiente imagen
-    }, 10000); // Cambia imagen cada 10 segundos (actualizado a 10000 milisegundos)
+    }, 6000); // Cambia imagen cada 10 segundos (actualizado a 10000 milisegundos)
 
     const btnAbrirRegistro = document.querySelector('#abrir-registro');
+    const btnAbrirRegistro_2 = document.querySelector('#abrir-registro-2');
     const btnCerrarRegistro = document.querySelector('#cerrar-registro');
     const modalRegistro = document.querySelector('#modal-registrar');
     const btnAbrirLogin = document.querySelector('#abrir-login');
     const btnCerrarLogin = document.querySelector('#cerrar-login');
     const modalLogin = document.querySelector('#modal-login');
-
-    // Evento para abrir el diálogo de registro
-    btnAbrirRegistro.addEventListener('click', () => {
+   
+    function abrirdialogo() {
         modalRegistro.showModal();
-    });
+    }
+    btnAbrirRegistro_2.addEventListener('click',abrirdialogo)
+    btnAbrirRegistro.addEventListener('click', abrirdialogo);
 
-    // Evento para cerrar el diálogo de registro
     btnCerrarRegistro.addEventListener('click', () => {
         modalRegistro.close();
     });
